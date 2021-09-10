59757f9afee5251c050bc4bf2240d317edbf69cd
RaulHerrera03 <A01254209@itesm.mx>
2021-09-10 17:39:20 +0000

    Parcial Practica 2

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,9 +1,12 @@
 
 
-def main():
-    numero = int(input("Escribe un numero : "))
-    #escribe tu código abajo de esta línea
 
+def main():
+    n = int(input("Escribe un numero : "))
+    r = 1
+    while n >=r**2:
+        r = r+1
+    print(r)
 
 if __name__=='__main__':
     main()
